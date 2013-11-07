c10874e2cc4376f90a9806d99ae3af84934a54c1
Juho Vepsalainen <bebraw@gmail.com>
2013-11-07 16:16:03 +0200

    Implement ltrim

---
+++ tests/ltrim.js
@@ -0,0 +1,16 @@
+var generate = require('annogenerate');
+var fuzz = require('annofuzz')(generate);
+var is = require('annois');
+
+var ltrim = require('../').ltrim;
+
+
+fuzz._amount = 100;
+fuzz(ltrim, function(op, chars, str) {
+    // TODO: figure out how to deal with invalid regex
+    var res = op(' ', str);
+
+    if(res.length > 0) return res;
+
+    return true;
+});
